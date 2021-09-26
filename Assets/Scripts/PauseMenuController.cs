60a3fe411bc3f2fa71cdc9d7dd1a33efa50af98f
Ghina Aramita <ghinaramita@gmail.com>
2021-09-27 01:28:18 +0700

    Menu and Pause Controller

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject panelPause;
+
+    public void PauseController()
+    {
+        if (Time.timeScale == 1)
+        {
+            panelPause.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            panelPause.SetActive(false);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void Menu()
+    {
+        Application.LoadLevel(0);
+    }
+}
